cf13c9a360127af368ba862f2069a25c89641ad7
mjgwon24 <alswlchlrh8@naver.com>
2025-01-22 15:14:26 +0900

    feat: page, search URL parameter 처리

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {useDebouncedCallback} from "use-debounce";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import {useSearchParams, usePathname, useRouter} from "next/navigation";
 
@@ -8,9 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const pathname = usePathname();
     const {replace} = useRouter();
 
-    function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term) => {
         const params = new URLSearchParams(searchParams);
-
+        params.set('page', '1');
         // Update the query parameter with the new search term.
         if (term) {
             params.set('query', term);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
